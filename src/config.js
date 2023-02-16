5a3fc8f2d9053a896bf6cf226e996306d52241fa
yellow-cube <93196588+yellow-cube@users.noreply.github.com>
2023-02-16 21:21:48 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
 
-//export const API = process.env.REACT_APP_API || 'https://icpulse.io/api/';
-export const API = process.env.REACT_APP_API || 'http://35.238.69.19/api/';
+export const API = process.env.REACT_APP_API || 'https://icpulse.io/api/';
 export const PADDING = 92;
